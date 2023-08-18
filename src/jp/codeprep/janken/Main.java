776e3cad28e7fe78cfcd2594cfb7ac73380ab3d5
coma-dwc <tamagomapon@gmail.com>
2023-08-18 21:04:13 +0900

    最終結果の表示

--- src/jp/codeprep/janken/Main.java
+++ src/jp/codeprep/janken/Main.java
@@ -7,6 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		Player player1 = new Player("Taro");
 		Player player2 = new Player("Hanako");
+		
+		//勝利数初期化
+		int player1win = 0;
+		int player2win = 0;
+		
 //		System.out.println(player1.getName());
 //		System.out.println(player2.getName());
 		for (int i=0; i<JANKEN_COUNT; i++) {
@@ -28,6 +33,23 @@ public class Main {
 				result  //resultを追記
 			)
 		);
+		if (hand1.winTo(hand2)) {
+			player1win++;
 		}
+		if (hand2.winTo(hand1)) {
+			player2win++;
+		}
+		}
+		String finalResult = player1win > player2win ?
+								player1.getName() + "の勝利" :
+							player2win > player1win ?
+								player2.getName() + "の勝利" : "引き分け";
+		System.out.println(
+				String.format("%s!!!(%s: %d - %d :%s)",
+					finalResult,
+					player1.getName(), player1win,
+					player2win, player2.getName()
+				)
+		);
 	}
 }
